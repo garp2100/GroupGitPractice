dc48ae983ea01829274e2434563e13156af2c167
Cbutler1 <collin.p.butler1@gmail.com>
2022-08-08 21:51:16 -0400

    adding bengal class

---
+++ GroupGitPractice/bengal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGitPractice
+{
+    internal class Bengal : Pet
+    {
+        public Bengal()
+        {
+            Name = "Decimal the bengal";
+        }
+        public override void MakeHappyNoise()
+        {
+            Console.WriteLine("Cat sounds");
+        }
+    }
+}
